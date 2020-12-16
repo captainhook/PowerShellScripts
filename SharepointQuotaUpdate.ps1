2fce112a93468732cf8e8d232ee4fbbd7bbce4ef
captainhook <16797541+captainhook@users.noreply.github.com>
2020-12-16 11:26:22 +0000

    Create SharepointQuotaUpdate.ps1

---
+++ SharepointQuotaUpdate.ps1
@@ -0,0 +1,18 @@
+Import-Module ExchangeOnlineManagement
+
+$UserCredential = Get-Credential
+Connect-ExchangeOnline -Credential $UserCredential -ShowProgress $true
+
+$AdminURL="https://x-admin.sharepoint.com" #change to your tentant
+Connect-SPOService -Url $AdminURL
+
+$StorageQuota = 102400
+$StorageQuotaWarningLevel = 81920
+
+Import-CSV -Path .\Sites.csv | ForEach-Object {
+	#Get Site URL from relevant column in CSV
+	$SiteURL = $_.SiteURL
+	
+	#Set Storage Quota
+	Set-SPOSite -Identity $SiteURL -StorageQuota $StorageQuota -StorageQuotaWarningLevel $StorageQuotaWarningLevel
+}
